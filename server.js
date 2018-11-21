99bdbe48f30b57bd3abcda95f9d6e7f3df1ca4d5
Gregor Aisch <gka@users.noreply.github.com>
2018-11-21 23:00:08 +0100

    base structure

---
+++ server.js
@@ -0,0 +1,7 @@
+// server.js
+const app = require('./src/app');
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log('Express server listening on port ' + port);
+});
