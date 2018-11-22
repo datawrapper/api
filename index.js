8d9cca098268d6bc07677f56e6efa3cde03b73f6
Gregor Aisch <gka@users.noreply.github.com>
2018-11-22 15:27:45 +0100

    added socket server

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ ORM.init(config.db);
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
 
-
-
-const server = app.listen(port, () => {
+const api_server = app.listen(port, () => {
   console.log('Express server listening on port ' + port);
 });
 
+const socket = require('./src/socket');
+
 // TLS SOCKET server
+socket.listen(config.socket.port, config.socket.host, () => {
+  	console.log("Socket listening at %s, on port %s", config.socket.host, config.socket.port);
+});
 
 
 // Websockets?
