daf93cfc46a87b65a64614c022265aa967d40a07
Gregor Aisch <gka@users.noreply.github.com>
2018-11-27 12:14:53 +0100

    refactoring

--- index.js
+++ index.js
@@ -4,20 +4,9 @@ const config = require('./config');
 ORM.init(config.db);
 
 // REST API
-const rest_app = require('./src/rest/app');
+const rest_app = require('./src/app');
 const port = process.env.PORT || 3000;
 const rest_server = rest_app.listen(port, () => {
   console.log('Express server listening on port ' + port);
 });
 
-// TLS SOCKET server
-const socket_app = require('./src/socket/server');
-socket_app.listen(config.socket.port, config.socket.host, () => {
-  	console.log("Socket server listening at %s, on port %s", config.socket.host, config.socket.port);
-});
-
-// Websockets?
-
-
-// Crons for maintainance
-require('./src/cron');
