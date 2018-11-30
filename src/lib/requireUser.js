5c14d057ce5b042189f445b450e0a4e4591a1f98
Gregor Aisch <gka@users.noreply.github.com>
2018-11-30 23:09:43 +0100

    send 401 if authorization is missing

--- src/lib/requireUser.js
+++ src/lib/requireUser.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
-	if (!res.user) {
-		return res.status(403).send({
-			error: 'This endpoint requires authentication'
-		});
-	}
-	next();
+    if (!res.user) {
+        return res.status(401).send({
+            error: 'This endpoint requires authentication'
+        });
+    }
+    next();
 };
