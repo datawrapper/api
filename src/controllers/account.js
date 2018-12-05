6afb03c9f44158f5334857377f4027727d86e547
Gregor Aisch <gka@users.noreply.github.com>
2018-12-05 18:47:47 +0100

    use serialized user data in account endpoint

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -3,7 +3,7 @@ const requireUser = require('../lib/requireUser');
 
 // show details of authenticated user
 router.get('/', requireUser, (req, res) => {
-    res.status(200).send({user: res.locals.user})
+    res.status(200).send({user: res.locals.user.serialize() })
 });
 
 module.exports = router;
