0aaedc553b9831d0615efb1220c3993637c32e26
Gregor Aisch <gka@users.noreply.github.com>
2019-01-24 20:48:14 +0100

    tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true
+    env: {
+        node: true,
+        commonjs: true
     },
-    "parser": "babel-eslint",
-    "extends": "eslint:recommended",
-    "globals": {
-        "Reflect": true,
-        "Promise": true
+    parser: 'babel-eslint',
+    extends: 'standard',
+    globals: {
+        Reflect: true,
+        Promise: true
     },
-    "rules": {
-        "no-sequences": "error",
-        "eqeqeq": ["error", "smart"],
-        "no-multiple-empty-lines": ["error", { "max": 2 }],
-        "no-console": ["error", { allow: ["warn", "error"] }]
+    rules: {
+        'no-sequences': 'error',
+        eqeqeq: ['error', 'smart'],
+        'no-multiple-empty-lines': ['error', { max: 2 }],
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        indent: ['error', 4],
+        semi: ['error', 'always']
     }
 };
