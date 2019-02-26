10da1fed9cdc2a413e1fc9821298608ddae4d3c7
Gregor Aisch <gka@users.noreply.github.com>
2019-02-26 14:09:37 +0100

    redirect /3/ requests to /v3/

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use(`/3`, require('./v3'));
+app.use('/3', (req, res) => {
+	res.redirect(303, '/v3' + req.path);
+});
+
+app.use(`/v3`, require('./v3'));
 
 // custom error handler
 app.use(function(err, req, res, next) {
-    if (res.headersSent) return next(err);
-    res.status(500).send({ error: err instanceof Error ? err.message : err });
+	if (res.headersSent) return next(err);
+	res.status(500).send({ error: err instanceof Error ? err.message : err });
 });
 
 app.get('/', (req, res) => {
-    res.status(200).send('api is running');
+	res.status(200).send('api is running');
 });
 
 // add other, non-v3 stuff here
