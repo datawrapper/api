446e0810de2509d146170eabe93e3469483f18c6
Fabian Gündel <f.guendel@gmail.com>
2019-03-07 11:44:40 +0100

    feat: `npm init` script

---
+++ create-api/index.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+/* eslint no-console: "off" */
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
+const findUp = require('find-up');
+
+const CWD = process.env.INIT_CWD || process.cwd();
+
+const pkg = {
+    name: 'dw-api',
+    version: '1.0.0',
+    scripts: {
+        start: 'dw-api',
+        sync: 'dw-sync'
+    },
+    dependencies: {
+        '@datawrapper/api': '2.0.0-alpha.3'
+    }
+};
+
+async function main() {
+    const configPath = await findUp('config.js');
+
+    if (!configPath) {
+        console.log(`
+❌ No config.js found!
+
+   Aborting API initialization.
+   Please follow the setup instructions for @datawrapper/api.
+
+   https://github.com/datawrapper/api#installation
+`);
+        process.exit();
+    }
+
+    const { plugins } = require(configPath);
+
+    fs.writeFileSync(path.join(CWD, 'package.json'), JSON.stringify(pkg, null, 4), {
+        encoding: 'utf-8'
+    });
+
+    const npm = spawn('npm', ['install', '-SE', '--production'].concat(plugins));
+
+    npm.stdout.on('data', data => process.stdout.write(data));
+    npm.stderr.on('data', data => process.stderr.write(data));
+}
+
+main();
