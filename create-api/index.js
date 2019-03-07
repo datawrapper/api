f3329b268ef75b5147b8155ceff6a5a7aa96e1df
Fabian Gündel <f.guendel@gmail.com>
2019-03-07 12:20:55 +0100

    update things

--- create-api/index.js
+++ create-api/index.js
@@ -37,9 +37,10 @@ async function main() {
         encoding: 'utf-8'
     });
 
+    console.log('[npm] Start package installation.');
     const npm = spawn(
         'npm',
-        ['install', '-SE', '--production', '@datawrapper/api@2.0.0-alpha.3'].concat(plugins)
+        ['install', '-SE', '--production', '@datawrapper/api@2.0.0-alpha.4'].concat(plugins)
     );
 
     npm.stdout.on('data', data => process.stdout.write(data));
@@ -48,6 +49,7 @@ async function main() {
     npm.on('close', code => {
         if (code) console.log(`Something went wrong. Code: ${code}`);
 
+        console.log('[npm] Rebuild packages.');
         const rebuild = spawn('npm', ['rebuild', '--build-from-source']);
 
         rebuild.stdout.on('data', data => process.stdout.write(data));
