fd90bd47cbf496e35be764ec1ca45cb7e1f78f3d
Fabian Gündel <f.guendel@gmail.com>
2019-03-11 16:25:07 +0100

    compat: teach login endpoint about the old ways

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,81 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const nanoid = require('nanoid');
+const Joi = require('joi');
 const Boom = require('boom');
 const { User, Session } = require('@datawrapper/orm/models');
 
+const DEFAULT_SALT = 'uRPAqgUJqNuBdW62bmq3CLszRFkvq4RW';
+
+const config = require('../../config');
+
+/**
+ * The old PHP API used sha256 to hash passwords with constant salts.
+ * The Node.js API uses bcrypt which is more secure.
+ * It is still important to support the old way for migration purposes since PHP and Node API
+ * will live side by side for some time.
+ * When the PHP Server gets turned off, we can hopefully delete this function.
+ *
+ * @deprecated
+ * @param {string} password - Password string sent from the client (Can be client side hashed or not)
+ * @param {string} passwordHash - Password hash to compare (from DB)
+ * @returns {boolean}
+ */
+function oldSchoolLogin(password, passwordHash) {
+    function oldschoolHash(pwhash, secret) {
+        const hmac = crypto.createHmac('sha256', secret);
+        hmac.update(pwhash);
+        return hmac.digest('hex');
+    }
+
+    let serverHash = oldschoolHash(password, config.api.secretAuthSalt);
+
+    if (serverHash === passwordHash) return true;
+
+    const clientHash = oldschoolHash(password, config.api.authSalt || DEFAULT_SALT);
+    serverHash = oldschoolHash(clientHash, config.api.secretAuthSalt || '');
+
+    return serverHash === passwordHash;
+}
+
+module.exports = {
+    name: 'auth-routes',
+    version: '1.0.0',
+    register: (server, options) => {
+        server.route({
+            method: 'POST',
+            path: '/login',
+            config: {
+                tags: ['api'],
+                auth: false,
+                validate: {
+                    payload: {
+                        email: Joi.string()
+                            .email()
+                            .required(),
+                        password: Joi.string().required()
+                    }
+                }
+            },
+            handler: login
+        });
+
+        server.route({
+            method: 'POST',
+            path: '/logout',
+            config: {
+                tags: ['api'],
+                auth: 'session'
+            },
+            handler: logout
+        });
+    }
+};
+
 async function login(request, h) {
+    const { email, password } = request.payload;
     const user = await User.findOne({
-        where: { email: request.payload.email },
+        where: { email },
         attributes: ['id', 'pwd']
     });
 
@@ -13,7 +83,18 @@ async function login(request, h) {
         return Boom.unauthorized('Invalid credentials');
     }
 
-    const isValid = await bcrypt.compare(request.payload.password, user.pwd);
+    let isValid = false;
+
+    /**
+     * Bcrypt uses a prefix for versioning. That way a bcrypt hash can be identified with "$2".
+     * https://en.wikipedia.org/wiki/Bcrypt#Description
+     */
+    if (user.pwd.startsWith('$2')) {
+        isValid = await bcrypt.compare(password, user.pwd);
+    } else {
+        isValid = oldSchoolLogin(password, user.pwd);
+    }
+
     if (!isValid) {
         return Boom.unauthorized('Invalid credentials');
     }
@@ -39,8 +120,3 @@ async function logout(request, h) {
         .unstate('DW-SESSION')
         .header('Clear-Site-Data', '"cookies", "storage", "executionContexts"');
 }
-
-module.exports = {
-    login,
-    logout
-};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,6 @@
-const Joi = require('joi');
-const { getAllUsers, getUser, editUser, createUser } = require('./users');
-const { login, logout } = require('./auth');
-
-const routes = {
-    pkg: require('../../package.json'),
+module.exports = {
+    name: 'routes',
+    version: '1.0.0',
     register: (server, options) => {
         server.route({
             method: 'GET',
@@ -15,128 +12,16 @@ const routes = {
             handler: (request, h) => h.redirect('v3/open-api.json')
         });
 
-        server.route({
-            method: 'POST',
-            path: '/auth/login',
-            config: {
-                tags: ['api'],
-                auth: false,
-                validate: {
-                    payload: {
-                        email: Joi.string()
-                            .email()
-                            .required(),
-                        password: Joi.string()
-                            .min(8)
-                            .required()
-                    }
-                }
-            },
-            handler: login
+        server.register(require('./users'), {
+            routes: {
+                prefix: '/users'
+            }
         });
 
-        server.route({
-            method: 'POST',
-            path: '/auth/logout',
-            config: {
-                tags: ['api'],
-                auth: 'simple'
-            },
-            handler: logout
-        });
-
-        server.route({
-            method: 'GET',
-            path: '/users',
-            config: {
-                tags: ['api'],
-                validate: {
-                    query: {
-                        search: Joi.string(),
-                        order: Joi.string()
-                            .uppercase()
-                            .valid(['ASC', 'DESC'])
-                            .default('ASC'),
-                        orderBy: Joi.string()
-                            .valid(['id', 'email', 'name', 'createdAt'])
-                            .default('id'),
-                        limit: Joi.number()
-                            .integer()
-                            .default(100),
-                        offset: Joi.number().integer()
-                    }
-                }
-            },
-            handler: getAllUsers
-        });
-
-        server.route({
-            method: 'GET',
-            path: '/users/{id}',
-            config: {
-                tags: ['api'],
-                validate: {
-                    params: {
-                        id: Joi.number().required()
-                    }
-                }
-            },
-            handler: getUser
-        });
-
-        server.route({
-            method: 'PATCH',
-            path: '/users/{id}',
-            config: {
-                tags: ['api'],
-                validate: {
-                    params: {
-                        id: Joi.number().required()
-                    },
-                    payload: {
-                        name: Joi.string(),
-                        email: Joi.string().email(),
-                        role: Joi.string().valid([
-                            'admin',
-                            'editor',
-                            'pending',
-                            'guest',
-                            'sysadmin',
-                            'graphic-editor'
-                        ]),
-                        language: Joi.string()
-                    }
-                }
-            },
-            handler: editUser
-        });
-
-        server.route({
-            method: 'POST',
-            path: '/users',
-            config: {
-                tags: ['api'],
-                validate: {
-                    payload: {
-                        name: Joi.string(),
-                        email: Joi.string()
-                            .email()
-                            .required(),
-                        role: Joi.string().valid([
-                            'admin',
-                            'editor',
-                            'pending',
-                            'guest',
-                            'sysadmin',
-                            'graphic-editor'
-                        ]),
-                        language: Joi.string()
-                    }
-                }
-            },
-            handler: createUser
+        server.register(require('./auth'), {
+            routes: {
+                prefix: '/auth'
+            }
         });
     }
 };
-
-module.exports = routes;
