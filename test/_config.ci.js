ea0bd91e190f2fda0338c51b66cec14725a39645
Fabian Gündel <f.guendel@gmail.com>
2019-03-13 15:32:22 +0100

    ci: test config

---
+++ test/_config.ci.js
@@ -0,0 +1,14 @@
+module.exports = {
+    api: {
+        domain: 'localhost',
+        sid: 'DW-SESSION'
+    },
+    db: {
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+};
