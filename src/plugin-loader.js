780ce4435b76a6faaf675e3ae9ced208a14617cc
Fabian Gündel <f.guendel@gmail.com>
2019-03-22 11:27:05 +0100

    feat: improved plugin loading with `config.js` as single source of truth

--- src/plugin-loader.js
+++ src/plugin-loader.js
@@ -1,10 +1,3 @@
-/*
-/src/plugins/{name}/index.js (file)
-
-@datawrapper/plugin-* | dw-plugin-* (npm)
-  /index.js
-*/
-const path = require('path');
 const glob = require('glob');
 const models = require('@datawrapper/orm/models');
 const findUp = require('find-up');
@@ -13,49 +6,69 @@ const get = require('lodash/get');
 const pkg = require(findUp.sync('package.json'));
 const config = require(findUp.sync('config.js'));
 
-async function loadPlugins(options) {
-    const paths = glob.sync('plugins/*/index.js');
+const localPluginPaths = glob.sync('plugins/*/index.js', { absolute: true });
+const localPlugins = localPluginPaths.map(require);
 
-    const localPlugins = paths.map(p => {
-        const plugin = require(path.join(process.cwd(), p));
+function findPlugin(name) {
+    const pluginObject = {
+        options: {
+            models,
+            config: get(config, ['plugins', name], {})
+        }
+    };
+
+    if (pkg.dependencies[name]) {
+        pluginObject.plugin = require(name);
+        pluginObject.type = 'npm';
+    }
+
+    const plugin = localPlugins.find(plugin => plugin.name === name);
+    if (plugin) {
+        pluginObject.plugin = plugin;
+        pluginObject.type = 'local';
+    } else {
         return {
-            plugin,
-            type: 'local',
-            options: {
-                models: options.models,
-                config: get(config, ['plugins', plugin.name], {})
-            }
+            error: name
         };
-    });
+    }
 
-    const npmPlugins = Object.keys(pkg.dependencies)
-        .filter(dep => dep.includes('@datawrapper/plugin-') || dep.includes('dw-plugin'))
-        .map(p => {
-            const plugin = require(p);
-            return {
-                plugin,
-                type: 'npm',
-                options: {
-                    models: options.models,
-                    config: get(config, ['plugins', plugin.name], {})
-                }
-            };
-        });
-
-    return npmPlugins.concat(localPlugins);
+    return pluginObject;
 }
 
+let loadingError = false;
 module.exports = {
     name: 'plugin-loader',
     version: '1.0.0',
     register: async (server, options) => {
-        const plugins = await loadPlugins({ models, config: config.plugins });
+        const plugins = Object.keys(config.plugins).map(findPlugin);
 
         if (plugins.length) {
-            await server.register(plugins);
-            plugins.forEach(({ plugin, type }) => {
-                server.logger().info({ version: plugin.version, type }, `[Plugin] ${plugin.name}`);
+            plugins.forEach(({ plugin, type, error }) => {
+                if (error) {
+                    loadingError = true;
+                    server.logger().error(`[Plugin] ${error}`, logError(error));
+                } else {
+                    server
+                        .logger()
+                        .info({ version: plugin.version, type }, `[Plugin] ${plugin.name}`);
+                }
             });
+            if (loadingError) process.exit(1);
+
+            await server.register(plugins);
         }
     }
 };
+
+function logError(name) {
+    return `
+
+Loading plugin [${name}] failed! Maybe it is not properly installed.
+
+Is it specified in "package.json" under "dependencies"?
+    If it is, try running "npm install"
+
+Is it available in "plugins/"?
+    Tip: run "grep -r "${name}" plugins/*/index.js"
+`;
+}
