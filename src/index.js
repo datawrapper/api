e68b327eb067878b1c164fab674be4009b7128e1
David Kokkelink <david.kokkelink@gmail.com>
2019-03-22 03:11:14 +0100

    await db

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const { version } = require('../package.json');
 logger.info(`starting @datawrapper/api v${version}`);
 
 // initialize database
-ORM.init(config);
+ORM.init(config).then(() => {
+    // register api plugins with core db
+    const Plugin = require('@datawrapper/orm/models/Plugin');
+    Plugin.register('datawrapper-api', Object.keys(config.plugins));
 
-// register api plugins with core db
-const Plugin = require('@datawrapper/orm/models/Plugin');
-Plugin.register('datawrapper-api', Object.keys(config.plugins));
+    // REST API
+    const app = require('./app');
+    const port = config.api && config.api.port ? config.api.port : process.env.PORT || 3000;
 
-// REST API
-const app = require('./app');
-const port = config.api && config.api.port ? config.api.port : process.env.PORT || 3000;
-
-app.listen(port, () => {
-    logger.info('API server listening on port ' + port);
+    app.listen(port, () => {
+        logger.info('API server listening on port ' + port);
+    });
 });
