8a25cab62d4c3427ca3f609d0cc066b04796e998
Fabian Gündel <f.guendel@gmail.com>
2019-04-17 16:34:07 +0200

    little bit of docs

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,13 +1,27 @@
 const EventEmitter = require('events');
 
+/**
+ * Custom event emitter that collects results of event listeners
+ *
+ * @class ApiEventEmitter
+ * @extends {EventEmitter}
+ */
 class ApiEventEmitter extends EventEmitter {
-    async emit(event, obj = {}) {
+    /**
+     * Emit function that calls all listeners and returns Promise of their results
+     *
+     * @param {string} event - Name of event to emit
+     * @param {any} [data] - Data to pass to event listeners
+     * @return {Promise} - Promise of event results as array
+     * @memberof ApiEventEmitter
+     */
+    async emit(event, data) {
         const listeners = this.listeners(event);
 
         const result = listeners.map(async func => {
             try {
-                const data = await func(obj);
-                return { status: 'success', data };
+                const result = await func(data);
+                return { status: 'success', data: result };
             } catch (error) {
                 return { status: 'error', error };
             }
