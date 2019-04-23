76a4b850489592a42b0ff49257adc5631c8373c4
Fabian Gündel <f.guendel@gmail.com>
2019-04-23 11:16:04 +0200

    feat: use events in s3 plugin

--- plugins/chart-data-s3/index.js
+++ plugins/chart-data-s3/index.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const Boom = require('boom');
-const Joi = require('joi');
 const S3 = require('aws-sdk/clients/s3');
 
 const s3 = new S3({ apiVersion: '2006-03-01' });
@@ -9,92 +7,30 @@ module.exports = {
     name: 'chart-data-s3',
     version: '1.0.0',
     register: (server, options) => {
-        server.route({
-            method: 'GET',
-            path: '/charts/{id}/data',
-            options: {
-                tags: ['api', 'plugin'],
-                validate: {
-                    params: Joi.object().keys({
-                        id: Joi.string()
-                            .length(5)
-                            .required()
-                    })
-                }
-            },
+        const { events, event } = server.app;
 
-            handler: getChartData
-        });
+        events.on(event.GET_CHART_DATA, getChartData);
+        events.on(event.PUT_CHART_DATA, writeChartData);
 
-        async function getChartData(request, h) {
-            const { id } = request.params;
-            const { Chart } = options.models;
-
-            const chart = await Chart.findByPk(id, {
-                attributes: ['author_id', 'created_at']
-            });
-
-            if (!chart) {
-                return Boom.notFound();
-            }
-
-            if (chart.author_id !== request.auth.artifacts.id) {
-                return Boom.unauthorized();
-            }
-
+        async function getChartData(chart) {
             const data = await s3
                 .getObject({
                     Bucket: options.config.bucket,
-                    Key: path.join(options.config.path, getDataPath(id, chart.created_at))
+                    Key: path.join(options.config.path, getDataPath(chart.id, chart.created_at))
                 })
                 .promise();
 
-            return h
-                .response(data.Body)
-                .header('Content-Type', 'text/csv')
-                .header('Content-Disposition', `attachment; filename=${id}.csv`);
+            return data.Body;
         }
 
-        server.route({
-            method: 'PUT',
-            path: '/charts/{id}/data',
-            options: {
-                tags: ['api', 'plugin'],
-                validate: {
-                    params: Joi.object().keys({
-                        id: Joi.string()
-                            .length(5)
-                            .required()
-                    }),
-                    payload: Joi.string()
-                }
-            },
-            handler: writeChartData
-        });
-
-        async function writeChartData(request, h) {
-            const { id } = request.params;
-            const { Chart } = options.models;
-
-            const chart = await Chart.findByPk(id, {
-                attributes: ['author_id', 'created_at']
-            });
-
-            if (!chart) {
-                return Boom.notFound();
-            }
-
-            if (chart.author_id !== request.auth.artifacts.id) {
-                return Boom.unauthorized();
-            }
-
+        async function writeChartData({ chart, data }) {
             let fileExists = false;
 
             try {
                 await s3
                     .headObject({
                         Bucket: options.config.bucket,
-                        Key: path.join(options.config.path, getDataPath(id, chart.created_at))
+                        Key: path.join(options.config.path, getDataPath(chart.id, chart.created_at))
                     })
                     .promise();
                 fileExists = true;
@@ -102,22 +38,17 @@ module.exports = {
                 fileExists = false;
             }
 
-            try {
-                await s3
-                    .putObject({
-                        ACL: 'public-read',
-                        Body: request.payload,
-                        Bucket: options.config.bucket,
-                        Key: path.join(options.config.path, getDataPath(id, chart.created_at)),
-                        ContentType: 'text/csv'
-                    })
-                    .promise();
-            } catch (error) {
-                request.logger.error(error.message);
-                return Boom.badGateway();
-            }
+            await s3
+                .putObject({
+                    ACL: 'public-read',
+                    Body: data,
+                    Bucket: options.config.bucket,
+                    Key: path.join(options.config.path, getDataPath(chart.id, chart.created_at)),
+                    ContentType: 'text/csv'
+                })
+                .promise();
 
-            return h.response().code(fileExists ? 204 : 201);
+            return { code: fileExists ? 204 : 201 };
         }
     }
 };
