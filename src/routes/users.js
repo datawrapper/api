91b3a93cec1d48936ab6563ba58060b27bfb069b
Fabian Gündel <f.guendel@gmail.com>
2019-06-27 12:41:06 +0200

    feat: trigger user setup

--- src/routes/users.js
+++ src/routes/users.js
@@ -97,6 +97,22 @@ module.exports = {
 
         server.route({
             method: 'POST',
+            path: '/{id}/setup',
+            options: {
+                tags: ['api'],
+                validate: {
+                    params: {
+                        id: Joi.number()
+                            .required()
+                            .description('User ID')
+                    }
+                }
+            },
+            handler: handleSetup
+        });
+
+        server.route({
+            method: 'POST',
             path: '/',
             options: {
                 auth: false,
@@ -403,4 +419,31 @@ async function deleteUser(request, h) {
     }
 
     return response;
+}
+
+async function handleSetup(request, h) {
+    const { params, server } = request;
+    const { generateToken, isAdmin, config } = server.methods;
+
+    if (!isAdmin(request)) return Boom.unauthorized();
+
+    const user = await User.findByPk(params.id, { attributes: ['id', 'email', 'language'] });
+
+    if (!user) return Boom.notFound();
+
+    const token = generateToken();
+
+    await user.update({ pwd: '', activate_token: token });
+
+    const { https, domain } = config('frontend');
+    await server.app.events.emit(request.server.app.event.SEND_EMAIL, {
+        type: 'user-setup',
+        to: user.email,
+        language: user.language,
+        data: {
+            invite_link: `${https ? 'https' : 'http'}://${domain}/account/invite/${token}`
+        }
+    });
+
+    return { token };
 }
