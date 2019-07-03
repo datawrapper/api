e4f1858607d1c1629e374683e7562cb68706b1bb
Fabian Gündel <f.guendel@gmail.com>
2019-07-03 14:11:39 +0200

    tooling: use new API DB

--- test/_config.ci.js
+++ test/_config.ci.js
@@ -23,7 +23,7 @@ module.exports = {
             port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
+            database: process.env.DB_NAME_API
         }
     }
 };
