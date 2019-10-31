f3c6fdd51ae80e157a7ad43757d33fdb439f0b77
Gregor Aisch <gka@users.noreply.github.com>
2019-10-31 15:34:10 +0100

    for now only admins can create teams

--- src/routes/teams.js
+++ src/routes/teams.js
@@ -860,6 +860,8 @@ async function deleteTeamMember(request, h) {
 async function createTeam(request, h) {
     const { auth, payload, server } = request;
     const isAdmin = server.methods.isAdmin(request);
+    // for now only admins can create teams
+    if (!isAdmin) return Boom.unauthorized();
 
     async function unusedId(name) {
         async function isUsed(id) {
