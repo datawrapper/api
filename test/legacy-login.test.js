b97d8ee928ade5821d42e945b6e3b65803033038
Fabian Gündel <f.guendel@gmail.com>
2019-11-26 12:08:09 +0100

    fix: safer chart associations

--- test/legacy-login.test.js
+++ test/legacy-login.test.js
@@ -1,64 +1,37 @@
 import test from 'ava';
-import nanoid from 'nanoid';
-import { Op } from 'sequelize';
-
 import { setup } from './helpers/setup';
 
-test.before(async t => {
-    const { server, models } = await setup({ usePlugins: false });
-    t.context.server = server;
-    t.context.users = [];
-    t.context.userIds = [];
+const USER_PASSWORD = 'legacy';
+/**
+ * USER_PASSWORD -> legacyHash(USER_PASSWORD, DEFAULT_SALT) ->
+ * CLIENT_HAST
+ */
+const CLIENT_HASH = '9574e9327ce73d61c8f5ffdc710e5c21cb07fcb3bef9ce4d892b88e6ad0ee107';
+/**
+ * USER_PASSWORD -> legacyHash(USER_PASSWORD, DEFAULT_SALT) ->
+ * output -> legacyHash(output, SECRET_AUTH_SALT) ->
+ * LEGACY_HASH
+ */
+const LEGACY_HASH = 'd51315c3623087d1c4378927b87b80e8e1a3216eb3976a4752e008fe82aff176';
 
-    const { User, Session } = models;
-    t.context.UserModel = User;
-    t.context.SessionModel = Session;
+test.before(async t => {
+    const { server, getUser } = await setup({ usePlugins: false });
+    t.context.server = server;
+    t.context.getUser = getUser;
 });
 
 test.beforeEach(async t => {
-    t.context.id = nanoid(5);
-    t.context.userEmail = `legacy-login-${t.context.id}@test.de`;
-
-    t.context.user = await t.context.UserModel.create({
-        email: t.context.userEmail,
-        pwd: 'fe2cbb87381c35e2ad4081d9dd23e2e160d7e2995a8d1110bdba1c4e2720704c',
-        role: 'editor'
-    });
-
-    t.context.userEmail = `legacy-login-${t.context.id}@test.de`;
+    const { user } = await t.context.getUser('editor', LEGACY_HASH);
+    t.context.userEmail = user.email;
 });
 
-test.afterEach.always(async t => {
-    t.context.users.push(t.context.userEmail);
-    t.context.userIds.push(t.context.user.id);
-});
-
-test.after.always(async t => {
-    const deletedUsers = await t.context.UserModel.destroy({
-        where: {
-            email: { [Op.or]: t.context.users }
-        }
-    });
-
-    const deletedSessions = await t.context.SessionModel.destroy({
-        where: {
-            data: {
-                [Op.or]: t.context.userIds.map(id => ({ [Op.like]: `dw-user-id|i:${id}%` }))
-            }
-        }
-    });
-
-    t.log('Users cleaned up:', deletedUsers);
-    t.log('Sessions cleaned up:', deletedSessions);
-});
-
-test.skip('Client hashed password', async t => {
+test('Client hashed password', async t => {
     const res = await t.context.server.inject({
         method: 'POST',
         url: '/v3/auth/login',
         payload: {
             email: t.context.userEmail,
-            password: 'ac6569a68bf6697a1f6072cc9f30e061ed41a234d12f824099ba84294233a855'
+            password: CLIENT_HASH
         }
     });
 
@@ -66,13 +39,13 @@ test.skip('Client hashed password', async t => {
     t.is(res.statusCode, 200);
 });
 
-test.skip('Non hashed password', async t => {
+test('Non hashed password', async t => {
     const res = await t.context.server.inject({
         method: 'POST',
         url: '/v3/auth/login',
         payload: {
             email: t.context.userEmail,
-            password: 'legacy'
+            password: USER_PASSWORD
         }
     });
 
@@ -80,13 +53,13 @@ test.skip('Non hashed password', async t => {
     t.is(res.statusCode, 200);
 });
 
-test.skip('Migrate client hashed password to new hash', async t => {
+test('Migrate client hashed password to new hash', async t => {
     let res = await t.context.server.inject({
         method: 'POST',
         url: '/v3/auth/login',
         payload: {
             email: t.context.userEmail,
-            password: 'ac6569a68bf6697a1f6072cc9f30e061ed41a234d12f824099ba84294233a855'
+            password: CLIENT_HASH
         }
     });
 
@@ -98,7 +71,7 @@ test.skip('Migrate client hashed password to new hash', async t => {
         url: '/v3/auth/login',
         payload: {
             email: t.context.userEmail,
-            password: 'legacy'
+            password: USER_PASSWORD
         }
     });
 
@@ -106,13 +79,13 @@ test.skip('Migrate client hashed password to new hash', async t => {
     t.is(res.statusCode, 200);
 });
 
-test.skip('Migrate password to new hash', async t => {
+test('Migrate password to new hash', async t => {
     let res = await t.context.server.inject({
         method: 'POST',
         url: '/v3/auth/login',
         payload: {
             email: t.context.userEmail,
-            password: 'legacy'
+            password: USER_PASSWORD
         }
     });
 
@@ -124,7 +97,7 @@ test.skip('Migrate password to new hash', async t => {
         url: '/v3/auth/login',
         payload: {
             email: t.context.userEmail,
-            password: 'legacy'
+            password: USER_PASSWORD
         }
     });
 
