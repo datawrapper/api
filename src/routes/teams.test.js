89c60ab82aaa2d77d8c86a64e4b8f5925708bf44
Gregor Aisch <gka@users.noreply.github.com>
2019-12-04 09:39:22 +0100

    remove .only from test
    Co-Authored-By: Fabian Gündel <f.guendel@gmail.com>

--- src/routes/teams.test.js
+++ src/routes/teams.test.js
@@ -201,7 +201,7 @@ test.only('[/v3/teams/:id] check that owners and admins can see owner, but membe
     t.is(teams.result.owner, undefined);
 });
 
-test.only('[/v3/teams/:id] check that owners and admins can see settings, but members cannot', async t => {
+test('[/v3/teams/:id] check that owners and admins can see settings, but members cannot', async t => {
     const { getTeamWithUser } = t.context;
     const { addUser, user: owner, session: ownerSession, team } = await getTeamWithUser();
     const { user: admin, session: adminSession } = await addUser('admin');
