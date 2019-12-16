1712ece0f0c7555efbd83ecd7c72f40cc7a15930
Fabian Gündel <f.guendel@gmail.com>
2019-12-16 14:53:28 +0100

    fix: revert joi update

--- src/routes/charts.js
+++ src/routes/charts.js
@@ -374,12 +374,12 @@ function register(server, options) {
                         .required()
                         .description('Full filename including extension.')
                 }),
-                payload: Joi.alternatives(
+                payload: [
                     Joi.string().description(
                         'An asset used by the chart such as CSV data or custom JSON map.'
                     ),
                     Joi.object()
-                )
+                ]
             },
             response: noContentResponse,
             payload: {
@@ -408,12 +408,12 @@ function register(server, options) {
                         .length(5)
                         .required()
                 }),
-                payload: Joi.alternatives(
+                payload: [
                     Joi.string().description(
                         'An asset used by the chart such as CSV data or custom JSON map.'
                     ),
                     Joi.object()
-                )
+                ]
             },
             response: noContentResponse,
             payload: {
