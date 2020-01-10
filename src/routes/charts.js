b89a6f83adfcaf8c1646b2c284a9022bebc435ac
David Kokkelink <david@datawrapper.de>
2020-01-10 18:51:33 +0100

    Fix joi validation for export endpoint (#96)
    * fix joi validation for export endpoint
    
    * allow auto as height
    
    * optional
    
    * min size
    
    * better validation
    
    Co-authored-by: Fabian Gündel <f.guendel@gmail.com>

--- src/routes/charts.js
+++ src/routes/charts.js
@@ -217,7 +217,9 @@ function register(server, options) {
                     unit: Joi.string().default('px'),
                     mode: Joi.string().default('rgb'),
                     width: Joi.number().default(600),
-                    height: Joi.any(),
+                    height: Joi.number()
+                        .min(1)
+                        .allow('auto'),
                     plain: Joi.boolean().default(false),
                     scale: Joi.number().default(1),
                     zoom: Joi.number().default(2),
@@ -257,12 +259,18 @@ function register(server, options) {
                 query: Joi.object({
                     unit: Joi.string().default('px'),
                     mode: Joi.string()
-                        .allow('rgb', 'cmyk')
+                        .valid('rgb', 'cmyk')
                         .default('rgb'),
-                    width: Joi.number().default(600),
-                    height: Joi.any(),
+                    width: Joi.number()
+                        .default(600)
+                        .min(1)
+                        .optional(),
+                    height: Joi.number()
+                        .min(1)
+                        .allow('auto'),
                     plain: Joi.boolean().default(false),
                     scale: Joi.number().default(1),
+                    zoom: Joi.number().default(2),
                     borderWidth: Joi.number(),
                     borderColor: Joi.string(),
                     download: Joi.boolean().default(false)
