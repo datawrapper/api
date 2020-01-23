cb215717b4e0968c3fbec26ba39ca443bb05345a
Gregor Aisch <gka@users.noreply.github.com>
2020-01-23 10:34:14 +0100

    load plugins individually and allow them to pass registration options (#104)
    * iterate plugins in for-loop instead of Promise.all
    * document routes.prefix in readme

--- src/plugin-loader.js
+++ src/plugin-loader.js
@@ -26,22 +26,21 @@ module.exports = {
         }
 
         if (plugins.length) {
-            await Promise.all(
-                plugins.map(async ({ plugin, options, error, name }) => {
-                    if (error) {
-                        server.logger().error(`[Plugin] ${error}`, logError(root, name));
-                        process.exit(1);
-                    } else {
-                        server.logger().info(
-                            {
-                                version: get(plugin, ['pkg', 'version'], plugin.version)
-                            },
-                            `[Plugin] ${get(plugin, ['pkg', 'name'], plugin.name)}`
-                        );
-                        await server.register({ plugin, options }, plugin.options);
-                    }
-                })
-            );
+            for (const { plugin, options, error, name } of plugins) {
+                if (error) {
+                    server.logger().error(`[Plugin] ${error}`, logError(root, name));
+                    process.exit(1);
+                } else {
+                    server.logger().info(
+                        {
+                            version: get(plugin, ['pkg', 'version'], plugin.version)
+                        },
+                        `[Plugin] ${get(plugin, ['pkg', 'name'], plugin.name)}`
+                    );
+
+                    await server.register({ plugin, options }, plugin.options);
+                }
+            }
         }
     }
 };
