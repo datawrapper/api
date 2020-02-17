f77b82d0a9b60bb7170ce3ca733d5f51df568abd
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-02-17 09:43:21 +0100

    fix: use domain as is and add dot

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,25 +13,4 @@ utils.generateToken = (length = 25) => {
 
 utils.noop = () => {};
 
-/**
- * returns the domain used for session cookies
- *
- * @param {object} api - the config.api section
- * @returns {string}
- */
-utils.cookieDomain = api => {
-    // allow manual override of cookie domain
-    if (api.cookieDomain) return api.cookieDomain;
-    // try to guess the cookie domain from api domain
-    // check how many parts the domain has
-    const parts = api.domain.split('.');
-    // for domains like 'localhost' we return the full
-    // api domain, thereby making the cookie impossible to
-    // access from the frontend
-    if (parts.length < 3) return api.domain;
-    // trim off first part and prepend a dot e.g.
-    // api.datawrapper.local --> .datawrapper.local
-    return '.' + parts.slice(1).join('.');
-};
-
 module.exports = utils;
