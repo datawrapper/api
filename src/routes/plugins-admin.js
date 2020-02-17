3c105b3a0756ba827d776a175420cafadab69cfe
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-02-17 16:12:51 +0100

    proper static file updates

--- src/routes/plugins-admin.js
+++ src/routes/plugins-admin.js
@@ -2,8 +2,10 @@ const stream = require('stream');
 const fs = require('fs-extra');
 const path = require('path');
 const { promisify } = require('util');
+const Joi = require('@hapi/joi');
 const Boom = require('@hapi/boom');
 const get = require('lodash/get');
+const intersection = require('lodash/intersection');
 const got = require('got');
 const tar = require('tar');
 
@@ -42,16 +44,22 @@ function register(server, options) {
 
     server.route({
         method: 'POST',
-        path: '/{name}',
+        path: '/update',
         options: {
-            auth: 'admin'
+            auth: 'admin',
+            validate: {
+                payload: {
+                    name: Joi.string().required(),
+                    branch: Joi.string().default('master')
+                }
+            }
         },
         handler: updatePlugin
     });
 
     async function updatePlugin(request, h) {
-        const { server, params, auth } = request;
-        const registration = server.registrations[params.name];
+        const { server, payload, auth } = request;
+        const registration = server.registrations[payload.name];
         const { api, general } = server.methods.config();
 
         if (!registration) {
@@ -63,27 +71,49 @@ function register(server, options) {
             return Boom.notImplemented();
         }
 
-        const destination = path.join(general.localPluginRoot, params.name);
-        const backupDestination = path.join(general.localPluginRoot, `${params.name}-backup`);
+        const pluginLocation = path.join(general.localPluginRoot, payload.name);
+        const backupFile = path.join(general.localPluginRoot, `${payload.name}-backup.tgz`);
 
-        request.logger.info({ user: auth.artifacts.id }, '[Start] Backup plugin', params.name);
-        await fs.copy(destination, backupDestination, {
-            filter: src => {
-                return !src.includes('node_modules/');
-            }
-        });
-        server.logger().info('[Done] Backup plugin', params.name);
-        server.logger().info('[Start] Update plugin', params.name);
+        const dir = await fs.readdir(pluginLocation);
 
+        /* Find directories to update */
+        const staticDirectories = intersection(dir, [
+            'less',
+            'locale',
+            'static'
+        ]); /* is this all?  */
+
+        request.logger.info({ user: auth.artifacts.id }, '[Start] Backup plugin', payload.name);
+
+        /* Create backup of current local directories */
+        await tar.create(
+            {
+                cwd: pluginLocation,
+                gzip: true,
+                file: backupFile
+            },
+            staticDirectories
+        );
+
+        server.logger().info('[Done] Backup plugin', payload.name);
+        server.logger().info('[Start] Update plugin', payload.name);
+
+        /* Download repo archive from Github and pipe it into node-tar to extract directories */
+        const staticDirectoriesRegex = new RegExp(`.*/(${staticDirectories.join('|')})/.*`);
         await pipeline(
-            got.stream(`${tarball}/node-publishing`, {
+            got.stream(`${tarball}/${payload.branch}`, {
                 headers: {
                     authorization: `token ${api.githubToken}`
                 }
             }),
-            tar.extract({ C: destination, strip: 1 })
+            tar.extract({
+                cwd: pluginLocation,
+                strip: 1,
+                filter: path => staticDirectoriesRegex.test(path)
+            })
         );
-        server.logger().info('[Done] Update plugin', params.name);
+
+        server.logger().info('[Done] Update plugin', payload.name);
 
         return h.response().code(204);
     }
