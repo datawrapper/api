ea37925e5d450eb29383ccef9f4695c7850f6678
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-03-04 12:31:26 +0100

    feat: add filter option to events.emit

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,5 +1,6 @@
 const EventEmitter = require('events');
 const { noop } = require('./index');
+
 /**
  * Custom event emitter that collects results of event listeners
  *
@@ -15,12 +16,13 @@ class ApiEventEmitter extends EventEmitter {
     /**
      * Emit function that calls all listeners and returns Promise of their results
      *
+     * @private
      * @param {string} event - Name of event to emit
      * @param {any} [data] - Data to pass to event listeners
      * @return {Promise} - Promise of event results as array
      * @memberof ApiEventEmitter
      */
-    async emit(event, data) {
+    async __private__emit(event, data) {
         if (!eventList[event]) {
             throw new TypeError(`Invalid event name (${event})`);
         }
@@ -41,6 +43,62 @@ class ApiEventEmitter extends EventEmitter {
         });
 
         return Promise.all(result);
+    }
+
+    /**
+     * Filter a list of event results
+     *
+     * @param {array} eventResults - List of event results
+     * @param {function|string} filter - Result filter
+     * @returns {array|object} - List or single event result
+     * @memberof ApiEventEmitter
+     */
+    filterEventResults(eventResults, filter) {
+        if (typeof filter === 'function') {
+            return eventResults.filter(filter);
+        }
+
+        if (filter === 'first') {
+            const firstResult = eventResults.find(r => r.status === 'success') || {};
+            return firstResult.data ? [firstResult.data] : [];
+        }
+
+        if (filter === 'success') {
+            return eventResults.filter(r => r.status === 'success').map(r => r.data);
+        }
+
+        return eventResults;
+    }
+
+    /**
+     * Emit function with options
+     *
+     * @param {string} event - Name of event to emit
+     * @param {any} [data] - Data to pass to event listeners
+     * @param {object} [options] - Options object to modify returned results
+     * @param {function|string} [options.filter] - Result filter
+     * @return {Promise} - Promise of event results
+     * @memberof ApiEventEmitter
+     */
+    async emit(event, data, options = {}) {
+        const results = await this.__private__emit(event, data);
+
+        let eventResults = results;
+        if (options.filter) {
+            eventResults = this.filterEventResults(eventResults, options.filter);
+
+            if (!eventResults.length) {
+                const errorResult = results.find(r => r.status === 'error');
+
+                if (errorResult) {
+                    throw errorResult.error;
+                }
+            }
+        }
+
+        if (options.filter === 'first') return eventResults[0];
+
+        return eventResults;
     }
 }
 
