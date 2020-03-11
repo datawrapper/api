eff2f70f917c6ee226f412dde5e21c1a87487441
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-03-11 15:11:16 +0100

    published query param (#129)

--- src/routes/charts.js
+++ src/routes/charts.js
@@ -95,7 +95,8 @@ function register(server, options) {
                         .description('5 character long chart ID.')
                 }),
                 query: Joi.object({
-                    withData: Joi.boolean()
+                    withData: Joi.boolean(),
+                    published: Joi.boolean()
                 })
             },
             response: chartResponse
@@ -559,6 +560,8 @@ function prepareChart(chart) {
     const { user, in_folder, ...dataValues } = chart.dataValues;
 
     return {
+        language: 'en_US',
+        theme: 'datawrapper',
         ...camelizeKeys(dataValues),
         folderId: in_folder,
         metadata: dataValues.metadata,
@@ -772,7 +775,7 @@ async function getChart(request, h) {
         isGuestChart ||
         (await chart.isEditableBy(auth.artifacts, auth.credentials.session));
 
-    if (!isEditable) {
+    if (query.published || !isEditable) {
         if (chart.published_at) {
             chart = await ChartPublic.findOne({
                 where: {
