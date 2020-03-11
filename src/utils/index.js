958244c64544f7b03a717383818110381fb8bb98
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-03-11 15:50:48 +0100

    hash files while copying

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,19 +16,46 @@ utils.stringify = obj => {
     });
 };
 
-utils.hashFile = async (filePath, hashLength = 8) => {
+function createHashedFileName(filePath, hash) {
+    const ext = path.extname(filePath);
+    const name = [path.basename(filePath, ext), hash].join('.');
+    return path.format({ name, ext });
+}
+
+utils.copyFileHashed = (filePath, destination, hashLength = 8) => {
+    let hash = crypto.createHash('sha256');
+    const outFilePath = path.join(destination, path.basename(filePath));
+    const input = fs.createReadStream(filePath);
+    const output = fs.createWriteStream(outFilePath);
+
+    input.pipe(output);
+    input.on('data', chunk => {
+        hash.update(chunk);
+    });
+
+    return new Promise((resolve, reject) => {
+        input.on('error', reject);
+        output.on('error', reject);
+
+        output.on('finish', rename);
+        async function rename() {
+            hash = hash.digest('hex').slice(0, hashLength);
+            const hashedFileName = createHashedFileName(filePath, hash);
+            await fs.move(outFilePath, path.join(destination, hashedFileName));
+            resolve(hashedFileName);
+        }
+    });
+};
+
+utils.readFileAndHash = async (filePath, hashLength = 8) => {
     const content = await fs.readFile(filePath, { encoding: 'utf-8' });
     let hash = crypto.createHash('sha256');
 
     hash.update(content);
     hash = hash.digest('hex').slice(0, hashLength);
 
-    const ext = path.extname(filePath);
     return {
-        fileName: path.format({
-            name: [path.basename(filePath, ext), hash].join('.'),
-            ext
-        }),
+        fileName: createHashedFileName(filePath, hash),
         content
     };
 };
