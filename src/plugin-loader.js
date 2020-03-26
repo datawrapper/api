cb4e7360f364735cec7edcfd5ede4b46a240e8f0
Gregor Aisch <gka@users.noreply.github.com>
2020-03-26 01:27:12 +0100

    load plugin locales and pass to chart bulk data endpoint (#135)
    * refactor: move default event handlers after plugin registration
    * feat: define output paths for asset files
    * read translations when loading plugins
    * pass chart translations with bulkData endpoint
    * use actual plugin name, not pkg name
    * refacor l10n.js
    * we have a solution now :)
    * add chart locales to publishData
    * pass chart locales to vis.locale
    * fix publicVersion increment
    
    Co-authored-by: Fabian Gündel <5798652+sto3psl@users.noreply.github.com>

--- src/plugin-loader.js
+++ src/plugin-loader.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
 const path = require('path');
 const models = require('@datawrapper/orm/models');
 const get = require('lodash/get');
+const { promisify } = require('util');
+const { addScope } = require('./utils/l10n');
+const readFile = promisify(fs.readFile);
+const readDir = promisify(fs.readdir);
 
 module.exports = {
     name: 'plugin-loader',
@@ -19,6 +24,7 @@ module.exports = {
                 const { routes, ...opts } = options;
                 return [
                     {
+                        name,
                         plugin,
                         options: {
                             models,
@@ -40,9 +46,29 @@ module.exports = {
                     server.logger().warn(`[Plugin] ${name}`, logError(root, name, error));
                 } else {
                     const version = get(plugin, ['pkg', 'version'], plugin.version);
-                    const name = get(plugin, ['pkg', 'name'], plugin.name);
                     server.logger().info(`[Plugin] ${name}@${version}`);
-
+                    // try to load locales
+                    try {
+                        const localePath = path.join(root, name, 'locale');
+                        const locales = await readDir(localePath);
+                        options.locales = {};
+                        for (let i = 0; i < locales.length; i++) {
+                            const file = locales[i];
+                            if (file === 'chart-translations.json') {
+                                // chart translations are special because they need to be passed
+                                // to the chart-core so they are availabe in rendered charts
+                                addScope(
+                                    'chart',
+                                    JSON.parse(await readFile(path.join(localePath, file)))
+                                );
+                            } else if (/[a-z]+_[a-z]+\.json/i.test(file)) {
+                                options.locales[file.split('.')[0]] = JSON.parse(
+                                    await readFile(path.join(localePath, file))
+                                );
+                            }
+                        }
+                        addScope(name, options.locales);
+                    } catch (e) {}
                     await server.register({ plugin, options }, pluginOptions);
                 }
             }
