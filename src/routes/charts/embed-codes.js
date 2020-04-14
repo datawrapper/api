495cd34e5dee92e73ea903ff088a9b13a0fd4cf4
Gregor Aisch <gka@users.noreply.github.com>
2020-04-14 17:53:47 +0200

    aria-label

--- src/routes/charts/embed-codes.js
+++ src/routes/charts/embed-codes.js
@@ -42,7 +42,15 @@ module.exports = (server, options) => {
             const __ = key => translate(key, { scope: 'core', language: auth.artifacts.language });
 
             const vis = server.app.visualizations.get(chart.type);
-            const ariaLabel = vis.ariaLabel || vis.title || __('chart');
+
+            const ariaLabel = vis.ariaLabel
+                ? // preferably, use the defined aria-label eg "Interactive line chart"
+                  translate(vis.ariaLabel, { scope: vis.__plugin, language: chart.language })
+                : vis.title
+                ? // otherwise fall back to the visualization title
+                  translate(vis.title, { scope: vis.__plugin, language: chart.language })
+                : // as last resort just use chart|map|table
+                  translate(vis.namespace || 'chart', { scope: 'core', language: chart.language });
 
             const team = await chart.getTeam();
             const preferred =
