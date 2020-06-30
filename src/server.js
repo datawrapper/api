aaf18b64e4ca72696b02e6378cac2ea14b7c8736
David Kokkelink <david@datawrapper.de>
2020-06-30 19:04:45 +0200

    fix require

--- src/server.js
+++ src/server.js
@@ -221,9 +221,7 @@ async function configure(options = { usePlugins: true, useOpenAPI: true }) {
     if (options.usePlugins) {
         await server.register([require('./plugin-loader')], routeOptions);
     }
-
-    await server.register([require('./v1')]);
-    await server.register([require('./v3')], routeOptions);
+    await server.register([require('./routes')], routeOptions);
 
     const { events, event } = server.app;
     const { general, frontend } = server.methods.config();
