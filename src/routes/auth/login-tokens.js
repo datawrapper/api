3188e686079d41c0f35ef2c7b7a671f5568896d5
David Kokkelink <david@datawrapper.de>
2020-07-07 17:46:51 +0200

    not required

--- src/routes/auth/login-tokens.js
+++ src/routes/auth/login-tokens.js
@@ -25,7 +25,6 @@ module.exports = async (server, options) => {
                         .required()
                         .description('A chart ID.'),
                     step: Joi.string()
-                        .required()
                         .optional()
                         .valid(
                             'edit',
