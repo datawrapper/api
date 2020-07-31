b8e4be77c57b124bb35a8bf274403fea6eb75e25
David Kokkelink <david@datawrapper.de>
2020-07-31 12:39:03 +0200

    chart.in_folder instead of inFolder

--- src/routes/charts/{id}/copy.js
+++ src/routes/charts/{id}/copy.js
@@ -91,7 +91,7 @@ module.exports = (server, options) => {
                 theme: srcChart.theme,
                 language: srcChart.language,
                 organization_id: srcChart.organization_id,
-                inFolder: srcChart.inFolder,
+                in_folder: srcChart.in_folder,
                 externalData: srcChart.externalData,
 
                 forked_from: srcChart.id,
@@ -102,7 +102,7 @@ module.exports = (server, options) => {
 
             if (isAdmin) {
                 newChart.organization_id = null;
-                newChart.inFolder = null;
+                newChart.in_folder = null;
             }
 
             const chart = await Chart.create(newChart);
