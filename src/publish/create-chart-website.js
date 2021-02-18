6ffdb397bd4667db990a30180d2bc76518d4b9cb
Elana <19191012+elanals@users.noreply.github.com>
2021-02-18 15:01:04 +0100

    use chart locale for <html> lang attribute

--- src/publish/create-chart-website.js
+++ src/publish/create-chart-website.js
@@ -217,6 +217,7 @@ module.exports = async function createChartWebsite(
      */
     const indexHTML = renderHTML({
         __DW_SVELTE_PROPS__: stringify(props),
+        CHART_LANGUAGE:locale.replace('_','-').split('-')[0],
         CHART_HTML: html,
         CHART_HEAD: head,
         POLYFILL_SCRIPT: getAssetLink(`../../lib/${polyfillScript}`),
