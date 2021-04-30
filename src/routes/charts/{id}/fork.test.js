9a159e1857e542f987f1c33e853d92ae8f5b92e6
Gregor Aisch <gka@users.noreply.github.com>
2021-04-30 15:04:11 +0200

    refine tests for /fork

--- src/routes/charts/{id}/fork.test.js
+++ src/routes/charts/{id}/fork.test.js
@@ -87,7 +87,7 @@ test("User can't fork an unpublished visualization", async t => {
     t.is(forkResponse.statusCode, 404);
 });
 
-test('User can fork chart, attributes match', async t => {
+test('User can fork fork-protected chart, attributes match', async t => {
     const { user, session } = await t.context.getUser();
     const { server } = t.context;
     const headers = {
@@ -103,8 +103,117 @@ test('User can fork chart, attributes match', async t => {
         externalData: 'https://static.dwcdn.net/data/12345.csv',
         forkable: true,
         metadata: {
+            describe: {
+                byline: 'Lorem Ipsum'
+            },
             visualize: {
                 basemap: 'us-counties'
+            }
+            // this is the default, so we don't need to set it
+            // publish: {
+            //     'protect-forks': true
+            // }
+        }
+    };
+
+    // create a new chart
+    const createResponse = await server.inject({
+        method: 'POST',
+        url: '/v3/charts',
+        headers,
+        payload: attributes
+    });
+
+    t.true(createResponse.result.forkable);
+
+    // upload some data
+    const dataResponse = await server.inject({
+        method: 'PUT',
+        url: `/v3/charts/${createResponse.result.id}/data`,
+        headers,
+        payload: 'foo,bar\n12,23'
+    });
+
+    t.is(dataResponse.statusCode, 204);
+
+    // create ChartPublic manually since /publish isn't working from tests yes
+    const { ChartPublic } = require('@datawrapper/orm/models');
+    await ChartPublic.create(decamelizeKeys(createResponse.result));
+
+    // fork new chart
+    const forkResponse = await server.inject({
+        method: 'POST',
+        url: `/v3/charts/${createResponse.result.id}/fork`,
+        headers
+    });
+
+    t.is(forkResponse.statusCode, 201);
+
+    const forkedChart = forkResponse.result;
+
+    const allMetadata = await server.inject({
+        method: 'GET',
+        url: `/v3/charts/${forkedChart.id}`,
+        headers
+    });
+
+    t.is(forkedChart.authorId, user.id);
+    t.is(forkedChart.forkedFrom, createResponse.result.id);
+    t.is(allMetadata.result.externalData, attributes.externalData);
+
+    const expectedAttributes = {
+        ...attributes,
+        theme: 'default',
+        language: 'en-US',
+        isFork: true,
+        forkable: undefined, // not returned from API,
+        metadata: {
+            ...attributes.metadata,
+            describe: {
+                intro: '',
+                'source-name': '',
+                'source-url': '',
+                'aria-description': '',
+                byline: '' // byline gets cleared since it's a protected fork
+            }
+        }
+    };
+
+    // compare attributes
+    for (var attr in expectedAttributes) {
+        if (attr === 'metadata') {
+            t.deepEqual(forkedChart.metadata.visualize, expectedAttributes.metadata.visualize);
+            t.deepEqual(forkedChart.metadata.describe, expectedAttributes.metadata.describe);
+        } else {
+            t.deepEqual(forkedChart[attr], expectedAttributes[attr], attr);
+        }
+    }
+});
+
+test('User can fork unprotected chart, attributes match', async t => {
+    const { user, session } = await t.context.getUser();
+    const { server } = t.context;
+    const headers = {
+        cookie: `DW-SESSION=${session.id}; crumb=abc`,
+        'X-CSRF-Token': 'abc',
+        referer: 'http://localhost'
+    };
+
+    const attributes = {
+        title: 'This is my chart',
+        theme: 'datawrapper-data',
+        language: 'en-IE',
+        externalData: 'https://static.dwcdn.net/data/12345.csv',
+        forkable: true,
+        metadata: {
+            describe: {
+                byline: 'Lorem Ipsum'
+            },
+            visualize: {
+                basemap: 'us-counties'
+            },
+            publish: {
+                'protect-forks': false
             }
         }
     };
@@ -158,13 +267,25 @@ test('User can fork chart, attributes match', async t => {
         ...attributes,
         theme: 'default',
         language: 'en-US',
-        forkable: undefined // not returned from API
+        isFork: undefined,
+        forkable: undefined, // not returned from API,
+        metadata: {
+            ...attributes.metadata,
+            describe: {
+                intro: '',
+                'source-name': '',
+                'source-url': '',
+                'aria-description': '',
+                byline: 'Lorem Ipsum' // byline remains
+            }
+        }
     };
 
     // compare attributes
-    for (var attr in attributes) {
+    for (var attr in expectedAttributes) {
         if (attr === 'metadata') {
             t.deepEqual(forkedChart.metadata.visualize, expectedAttributes.metadata.visualize);
+            t.deepEqual(forkedChart.metadata.describe, expectedAttributes.metadata.describe);
         } else {
             t.deepEqual(forkedChart[attr], expectedAttributes[attr], attr);
         }
